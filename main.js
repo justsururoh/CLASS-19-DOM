a25bb616605d385aa8cb4034d00f07b9468ab08e
Muhammed Sururat Omotoyosi <sururatomotoyosi@gmail.com>
2025-01-28 14:10:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const movieList = {
+    Horror: "The Nun",
+    Action: "RRR",
+    Comedy: "Mr Queen"
+}
+
+console.log(movieList);
+console.log(movieList.Action);
+
+document.body.style.backgroundColor = "gold"
+
+let a = document.getElementById(`a`);
+
+a.style.backgroundColor = "red"
+a.style.color = "green"
+
+let b = document.getElementById(`b`);
+
+b.style.backgroundColor = "red"
+b.style.color = "green"
+
+let c = document.getElementById(`c`);
+
+c.style.backgroundColor = "pink"
+c.style.color = "orange"
+
+let d = document.getElementById(`d`);
+
+d.style.backgroundColor = "brown"
+d.style.color = "red"
+
+let e = document.getElementById(`e`);
+
+e.style.backgroundColor = "grey"
+e.style.color = "green"
+
+let f = document.getElementById(`f`);
+
+f.style.backgroundColor = "wheat"
+f.style.color = "green"
+
+let g = document.getElementById(`g`);
+
+g.style.backgroundColor = "blue"
+g.style.color = "green"
+
